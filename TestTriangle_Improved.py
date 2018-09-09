fbd51c9d97c76f10d1a0038dca0ea5f4dca63a80
esanchez32 <37596612+esanchez32@users.noreply.github.com>
2018-09-08 22:39:41 -0400

    Add files via upload

---
+++ TestTriangle_Improved.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Updated Sep 8, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
+
+@author: Elena Sanchez
+@author: rk
+"""
+
+import unittest
+
+from Triangle import classifyTriangle
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+
+class TestTriangles(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5,3,4 is a Right triangle')
+        
+    def testEquilateralTriangles(self): 
+        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
+
+
+if TestTriangles == ():
+    print('Running unit tests')
+    unittest.TestTriangles()
+
